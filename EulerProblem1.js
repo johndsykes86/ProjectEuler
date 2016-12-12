665c8becfb5ae94569b50e728eaf91e2bb886ed7
Johnathon Sykes <johndsykes86@gmail.com>
2016-12-12 14:14:36 -0800

    corrected && to || for if conditional, added alert to give total

--- EulerProblem1.js
+++ EulerProblem1.js
@@ -1,9 +1,9 @@
 var total = 0;
 
-for (var i = 0; i<1000; i++){
-  if (i%3===0 && i%5===0) {
+for (var i = 0; i<1=000; i++){
+  if (i%3===0 || i%5===0) {
     total += i;
   }
 }
 
-console.log(total);
+alert('The total of all natural numbers that are also multiples of 3 and 5 is: '+total);
